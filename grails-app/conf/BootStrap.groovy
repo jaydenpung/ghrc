8df37278f104263b6bf5821e7675f0678374c63f
kahheng93 <kahheng93@gmail.com>
2018-04-07 00:36:19 +0800

    create default user if not exist

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,14 @@
+import ghrc.*
+
 class BootStrap {
 
     def init = { servletContext ->
+    	if (User.count() == 0) {
+    		User user = new User (
+    			username: 'admin',
+    			password: 'Ghrc1234'
+    		).save(flush: true, failOnError: true)
+    	}
     }
     def destroy = {
     }
